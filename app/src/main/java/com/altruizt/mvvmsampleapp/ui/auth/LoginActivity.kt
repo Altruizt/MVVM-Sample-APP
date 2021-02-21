d0b866f3274df14799dcd458cd29acdbc2e54716
gianginanjar1996 <gianginanjar1996@gmail.com>
2021-02-21 15:02:03 +0700

    Menambahkan res login_activity dan signup_activity desain + new drawable

--- app/src/main/java/com/altruizt/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/altruizt/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.altruizt.mvvmsampleapp
+package com.altruizt.mvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.altruizt.mvvmsampleapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
